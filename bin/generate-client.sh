25faf3ede38952e68d242d5a9cc1baf475ba8888
Taylor McKinnon <mail@tmacs.space>
2023-04-06 15:00:53 -0700

    impr(SCUBA-18): Add script to generate client

---
+++ bin/generate-client.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ ! -f "openapi.yaml" ]; then
+    printf 'No spec file found at %s/%s\n' "$PWD" "openapi.yaml"
+    exit 1
+fi
+
+export TS_POST_PROCESS_FILE="yarn format"
+
+exec yarn openapi-generator-cli generate  -i openapi.yaml -g typescript-axios -o src/
